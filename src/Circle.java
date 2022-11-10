9fb0af8f90aa48081b0727e46095dfbc21bafafc
Salymbek <calumbekcaatbekov@gmail.com>
2022-11-10 16:55:06 +0600

    commit

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Circle {
+public class Circle {
+    private double PI;
+    private int radius;
+
+    public double getPI() {
+        return PI;
+    }
+
+    public void setPI(double PI) {
+        this.PI = PI;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double PI, int radius) {
+        this.PI = PI;
+        this.radius = radius;
+    }
+
+   public double area(double PI, int radius){
+        return PI * (radius * radius);
+   }
+
+   public double circumference(double PI, int radius){
+        return PI * 2 * radius;
+   }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("RADIUS :");
+        int radius = scanner.nextInt();
+
+        Circle circle = new Circle(3.14159265359, radius);
+
+        System.out.println("AREA = " + circle.area(3.14159265359, radius));
+
+        System.out.println("CIRCUMFERENCE = " + circle.circumference(3.14159265359, radius));
+
     }
+
+
 }
